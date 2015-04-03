49dee4438b88757088bdc50d5373731b830b6d03
Huu Nguyen <whoshuu@gmail.com>
2015-04-03 15:15:50 -0700

    Add post tests

---
+++ test/post_tests.cpp
@@ -0,0 +1,49 @@
+#include <gtest/gtest.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include <cpr.h>
+
+#include "server.h"
+
+
+static Server* server;
+auto base = Url{"http://127.0.0.1:8080"};
+
+TEST(PostTests, PostSingleTest) {
+    auto url = Url{base + "/hello.html"};
+    auto response = cpr::Post(url, Payload{{"hello", "world"}});
+    auto expected_text = std::string{"Hello world!"};
+    EXPECT_EQ(expected_text, response.text);
+    EXPECT_EQ(url, response.url);
+    EXPECT_EQ(std::string{"text/html"}, response.header["content-type"]);
+    EXPECT_EQ(200, response.status_code);
+}
+
+TEST(PostTests, PostManyTest) {
+    auto url = Url{base + "/hello.html"};
+    auto response = cpr::Post(url, Payload{{"hello", "world"}, {"more", "values"}});
+    auto expected_text = std::string{"Hello world!"};
+    EXPECT_EQ(expected_text, response.text);
+    EXPECT_EQ(url, response.url);
+    EXPECT_EQ(std::string{"text/html"}, response.header["content-type"]);
+    EXPECT_EQ(200, response.status_code);
+}
+
+TEST(PostTests, PostBadHostTest) {
+    auto url = Url{"http://bad_host/"};
+    auto response = cpr::Post(url, Payload{{"hello", "world"}});
+    EXPECT_EQ(std::string{}, response.text);
+    EXPECT_EQ(url, response.url);
+    EXPECT_EQ(std::string{}, response.header["content-type"]);
+    EXPECT_EQ(0, response.status_code);
+}
+
+int main(int argc, char** argv) {
+    server = new Server();
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::AddGlobalTestEnvironment(server);
+    return RUN_ALL_TESTS();
+}
