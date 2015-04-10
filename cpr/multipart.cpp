df014fbec5aae0e955c42a8ce62f7487dd864139
Huu Nguyen <whoshuu@gmail.com>
2015-04-09 21:52:38 -0700

    Add multipart class

---
+++ cpr/multipart.cpp
@@ -0,0 +1,8 @@
+#include "multipart.h"
+
+
+Multipart::Multipart(const std::initializer_list<Part>& parts) {
+    for (auto part = parts.begin(); part != parts.end(); ++part) {
+        this->parts.push_back(*part);
+    }
+}
---
+++ include/multipart.h
@@ -0,0 +1,35 @@
+#ifndef MULTIPART_H
+#define MULTIPART_H
+
+#include <initializer_list>
+#include <string>
+#include <vector>
+
+
+struct File {
+    File(const std::string& filepath) : filepath{filepath} {}
+    std::string filepath;
+};
+
+struct Part {
+    Part(const std::string& name, const std::string& value, const std::string& content_type={})
+        : name{name}, value{value}, content_type{content_type}, is_file{false} {}
+    Part(const std::string& name, const int& value, const std::string& content_type={})
+        : name{name}, value{std::to_string(value)}, content_type{content_type}, is_file{false} {}
+    Part(const std::string& name, const File& file, const std::string& content_type={})
+        : name{name}, value{file.filepath}, content_type{content_type}, is_file{true} {}
+
+    std::string name;
+    std::string value;
+    std::string content_type;
+    bool is_file;
+};
+
+class Multipart {
+  public:
+    Multipart(const std::initializer_list<Part>& parts);
+
+    std::vector<Part> parts;
+};
+
+#endif
