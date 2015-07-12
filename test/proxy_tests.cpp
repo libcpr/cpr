e4430c275527f3532273fba73495f5371a28a64a
Huu Nguyen <whoshuu@gmail.com>
2015-07-12 15:43:55 -0400

    Add proxy tests References #5

---
+++ test/proxy_tests.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include <cpr.h>
+
+
+// TODO: This uses public servers for proxies and endpoints. This should be replaced with a source
+// code implementation inside server.cpp
+
+TEST(ProxyTests, SingleProxyTest) {
+    auto url = Url{"http://www.httpbin.org/get"};
+    auto response = cpr::Get(url, Proxies{{"http", "173.234.8.2:80"}});
+    EXPECT_EQ(url, response.url);
+    EXPECT_EQ(std::string{"application/json"}, response.header["content-type"]);
+    EXPECT_EQ(200, response.status_code);
+}
+
+TEST(ProxyTests, MultipleProxyTest) {
+    auto url = Url{"http://www.httpbin.org/get"};
+    auto response = cpr::Get(url, Proxies{{"http", "173.234.8.2:80"},
+                                          {"https", "67.195.42.72:80"}});
+    EXPECT_EQ(url, response.url);
+    EXPECT_EQ(std::string{"application/json"}, response.header["content-type"]);
+    EXPECT_EQ(200, response.status_code);
+}
+
+TEST(ProxyTests, ProxySessionTest) {
+    auto url = Url{"http://www.httpbin.org/get"};
+    Session session;
+    session.SetUrl(url);
+    session.SetProxies(Proxies{{"http", "173.234.8.2:80"}});
+    auto response = session.Get();
+    EXPECT_EQ(url, response.url);
+    EXPECT_EQ(std::string{"application/json"}, response.header["content-type"]);
+    EXPECT_EQ(200, response.status_code);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
