b71d0ded60e2ea9a6304d1fe32c5ff376a06a6bf
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-09-22 22:17:54 +0200

    std::vector can be constructed via initializer_list

--- cpr/multipart.cpp
+++ cpr/multipart.cpp
@@ -2,10 +2,6 @@
 
 namespace cpr {
 
-    Multipart::Multipart(const std::initializer_list<Part>& parts) {
-        for (auto part = parts.begin(); part != parts.end(); ++part) {
-            this->parts.push_back(*part);
-        }
-    }
-
+    Multipart::Multipart(const std::initializer_list<Part>& parts)
+        : parts{parts} { }
 }
