e69335bd132c6f7ea7ca9d53ec2ae528ae2b5c72
Huu Nguyen <whoshuu@gmail.com>
2015-09-24 13:22:13 -0700

    Use forwarding references in Response constructors References #50

--- include/response.h
+++ include/response.h
@@ -10,13 +10,16 @@ namespace cpr {
 class Response {
   public:
     Response() = default;
-    Response(const long& status_code, const std::string& text, const Header& header, const Url& url,
+    template <typename TextType, typename HeaderType, typename UrlType>
+    Response(const long& status_code, TextType&& text, HeaderType&& header, UrlType&& url,
              const double& elapsed)
-            : status_code{status_code}, text{text}, header{header}, url{url}, elapsed{elapsed} {};
-    Response(const long& status_code, const std::string& text, const Header& header, const Url& url,
-             const double& elapsed, const Cookies& cookies)
-            : status_code{status_code}, text{text}, header{header}, url{url}, elapsed{elapsed},
-              cookies{cookies} {};
+            : status_code{status_code}, text{CPR_FWD(text)}, header{CPR_FWD(header)},
+              url{CPR_FWD(url)}, elapsed{elapsed} {}
+    template <typename TextType, typename HeaderType, typename UrlType, typename CookiesType>
+    Response(const long& status_code, TextType&& text, HeaderType&& header, UrlType&& url,
+             const double& elapsed, CookiesType&& cookies)
+            : status_code{status_code}, text{CPR_FWD(text)}, header{CPR_FWD(header)},
+              url{CPR_FWD(url)}, elapsed{elapsed}, cookies{CPR_FWD(cookies)} {}
 
     long status_code;
     std::string text;
