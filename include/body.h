9b590204de5ba4507d19340ceca17e529915df42
Huu Nguyen <whoshuu@gmail.com>
2015-10-13 21:37:35 -0400

    Make Body an opaque typedef of std::string

--- include/body.h
+++ include/body.h
@@ -7,13 +7,23 @@
 
 namespace cpr {
 
-class Body {
+class Body : public std::string {
   public:
-    template <typename TextType>
-    Body(TextType&& p_text)
-            : text{CPR_FWD(p_text)} {}
-
-    std::string text;
+    Body() = default;
+    Body(const Body& rhs) = default;
+    Body(Body&& rhs) = default;
+    Body& operator=(const Body& rhs) = default;
+    Body& operator=(Body&& rhs) = default;
+    explicit Body(const char* raw_string) : std::string(raw_string) {}
+    explicit Body(const char* raw_string, size_t length) : std::string(raw_string, length) {}
+    explicit Body(size_t to_fill, char character) : std::string(to_fill, character) {}
+    explicit Body(const std::string& std_string) : std::string(std_string) {}
+    explicit Body(const std::string& std_string, size_t position, size_t length = std::string::npos)
+            : std::string(std_string, position, length) {}
+    explicit Body(std::initializer_list<char> il) : std::string(il) {}
+    template <class InputIterator>
+    explicit Body(InputIterator first, InputIterator last)
+            : std::string(first, last) {}
 };
 
 } // namespace cpr
