24ec4a602b851b1282fca6ca2966a6381a85f5f9
Chase Geigle <sky@skystrife.com>
2015-10-17 15:45:15 -0500

    Minor DRY refactoring for cookie encoding.

--- cpr/cookies.cpp
+++ cpr/cookies.cpp
@@ -10,14 +10,15 @@ Cookies::Cookies(const std::initializer_list<std::pair<const std::string, std::s
 std::string Cookies::GetEncoded() const {
     std::stringstream stream;
     for (const auto& item : map_) {
+        stream << cpr::util::urlEncode(item.first) << "=";
         // special case version 1 cookies, which can be distinguished by
         // beginning and trailing quotes
         if (!item.second.empty() && item.second.front() == '"' && item.second.back() == '"') {
-            stream << cpr::util::urlEncode(item.first) << "=" << item.second << "; ";
+            stream << item.second;
         } else {
-            stream << cpr::util::urlEncode(item.first) << "=" << cpr::util::urlEncode(item.second)
-                   << "; ";
+            stream << cpr::util::urlEncode(item.second);
         }
+        stream << "; ";
     }
     return stream.str();
 }
