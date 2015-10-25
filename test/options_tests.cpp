b52289ddca0601f2fe7583e0c1d0cdbe4b313a29
Huu Nguyen <whoshuu@gmail.com>
2015-10-25 17:03:41 -0400

    Add async tests for OPTIONS method References #34

--- test/options_tests.cpp
+++ test/options_tests.cpp
@@ -33,6 +33,40 @@ TEST(OptionsTests, SpecificUrlTest) {
     EXPECT_EQ(200, response.status_code);
 }
 
+TEST(OptionsTests, AsyncBaseUrlTest) {
+    auto url = Url{base + "/"};
+    std::vector<AsyncResponse> responses;
+    for (int i = 0; i < 10; ++i) {
+        responses.emplace_back(cpr::OptionsAsync(url));
+    }
+    for (auto& future_response : responses) {
+        auto response = future_response.get();
+        auto expected_text = std::string{""};
+        EXPECT_EQ(expected_text, response.text);
+        EXPECT_EQ(url, response.url);
+        EXPECT_EQ(std::string{"GET, POST, PUT, DELETE, PATCH, OPTIONS"},
+                  response.header["Access-Control-Allow-Methods"]);
+        EXPECT_EQ(200, response.status_code);
+    }
+}
+
+TEST(OptionsTests, AsyncSpecificUrlTest) {
+    auto url = Url{base + "/hello.html"};
+    std::vector<AsyncResponse> responses;
+    for (int i = 0; i < 10; ++i) {
+        responses.emplace_back(cpr::OptionsAsync(url));
+    }
+    for (auto& future_response : responses) {
+        auto response = future_response.get();
+        auto expected_text = std::string{""};
+        EXPECT_EQ(expected_text, response.text);
+        EXPECT_EQ(url, response.url);
+        EXPECT_EQ(std::string{"GET, OPTIONS"},
+                  response.header["Access-Control-Allow-Methods"]);
+        EXPECT_EQ(200, response.status_code);
+    }
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     ::testing::AddGlobalTestEnvironment(server);
