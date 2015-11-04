8f106cbd80ce01bb0274a2cbfd81c34450256fa6
Mexus <gilaldpellaeon@gmail.com>
2015-11-04 17:10:43 +0300

    Added "AddParameter" method to the "Parameters" class

--- cpr/parameters.cpp
+++ cpr/parameters.cpp
@@ -9,12 +9,16 @@ namespace cpr {
 
 Parameters::Parameters(const std::initializer_list<Parameter>& parameters) {
     for (const auto& parameter : parameters) {
-        if (!content.empty()) {
-            content += "&";
-        }
-        auto escaped = cpr::util::urlEncode(parameter.value);
-        content += parameter.key + "=" + escaped;
+        AddParameter(parameter);
     }
+}
+
+void Parameters::AddParameter(const Parameter& parameter) {
+    if (!content.empty()) {
+        content += "&";
+    }
+    auto escaped = cpr::util::urlEncode(parameter.value);
+    content += parameter.key + "=" + escaped;
 }
 
 } // namespace cpr
