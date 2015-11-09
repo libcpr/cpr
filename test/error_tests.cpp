a74f4f7a9a058603d8ad98c0207e86c52a265081
Don Goodman-Wilson <don@slack-corp.com>
2015-11-09 07:33:31 -0800

    Adding a runtime check for SSL support. This means that test coverage is dependent upon features selected at compile time…but then, there really isn't much that can be done about it.

--- test/error_tests.cpp
+++ test/error_tests.cpp
@@ -3,6 +3,8 @@
 #include <string>
 
 #include <cpr.h>
+#include <curl/curl.h>
+
 
 #include "server.h"
 
@@ -13,11 +15,24 @@ auto base = server->GetBaseUrl();
 auto baseSSL = server->GetBaseUrlSSL();
 
 TEST(ErrorTests, BasicSSLFailure) {
+    //If CURL has SSL enabled, we should expect SSL_CONECT_ERROR
+    //  If not, we shoujld expect UNSUPPORTED_PROTOCOL
+
+
     auto url = Url{baseSSL + "/hello.html"};
     auto response = cpr::Get(url);
     EXPECT_EQ(url, response.url);
     EXPECT_EQ(0, response.status_code);
-    EXPECT_EQ(ErrorCode::SSL_CONNECT_ERROR, response.error.code);
+
+    //This is ugly, because it presume CURL under the hood. Might be nice to have something in `cpr::Session`
+    //  That could tell us if SSL is supported.
+    auto curl_version = curl_version_info(CURLVERSION_NOW);
+    auto expected = ErrorCode::UNSUPPORTED_PROTOCOL;
+    if(curl_version->features & CURL_VERSION_SSL) {
+        expected = ErrorCode::SSL_CONNECT_ERROR;
+    }
+    EXPECT_EQ(expected, response.error.code);
+
 }
 
 //Not terribly sure how to test other SSL error codes
