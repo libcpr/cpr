d6969d71636aeb86a6a967d0aa3838463c6452cc
Huu Nguyen <whoshuu@gmail.com>
2015-12-11 11:26:21 -0500

    Style fix to Response constructor

--- include/response.h
+++ include/response.h
@@ -13,11 +13,14 @@ class Response {
   public:
     Response() = default;
 
-    template <typename TextType, typename HeaderType, typename UrlType, typename CookiesType, typename ErrorType>
+    template <typename TextType, typename HeaderType, typename UrlType, typename CookiesType,
+              typename ErrorType>
     Response(const long& p_status_code, TextType&& p_text, HeaderType&& p_header, UrlType&& p_url,
-             const double& p_elapsed, ErrorType&& p_error = Error{}, CookiesType&& p_cookies = Cookies{})
+             const double& p_elapsed, CookiesType&& p_cookies = Cookies{},
+             ErrorType&& p_error = Error{})
             : status_code{p_status_code}, text{CPR_FWD(p_text)}, header{CPR_FWD(p_header)},
-              url{CPR_FWD(p_url)}, elapsed{p_elapsed}, cookies{CPR_FWD(p_cookies)}, error{CPR_FWD(p_error)} {}
+              url{CPR_FWD(p_url)}, elapsed{p_elapsed}, cookies{CPR_FWD(p_cookies)},
+              error{CPR_FWD(p_error)} {}
 
     long status_code;
     std::string text;
