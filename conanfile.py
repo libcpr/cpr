6152f8b7ac6829058bd05b6473613113d56255dd
Don Goodman-Wilson <don@slack-corp.com>
2016-03-07 09:34:34 -0800

    Add package dependencies.

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,9 @@ from conans import ConanFile, CMake
 class CPRConan(ConanFile):
     name = "cpr"
     version = "1.2.0"
+    url = "https://github.com/whoshuu/cpr.git"
+    license = "MIT"
+    requires = "libcurl/7.47.1@lasote/stable"
     settings = "os", "compiler", "build_type", "arch"
     options = {"build_cpr_tests": [True, False],
                "insecure_curl": [True, False],
@@ -11,16 +14,30 @@ class CPRConan(ConanFile):
     default_options = "build_cpr_tests=False", "insecure_curl=False", "generate_coverage=False", "use_openssl=True"
 
     def source(self):
-        self.run("git clone https://github.com/DEGoodmanWilson/cpr.git")
+        self.run("git clone https://github.com/DEGoodmanWilson/cpr.git --branch conan")
+
+    def config(self):
+        if self.options.use_openssl:
+            self.options["libcurl"].with_openssl = True
+        else:
+            self.options["libcurl"].with_openssl = False
+
+        if self.options.build_cpr_tests:
+            self.requires.add("gtest/1.7.0@lasote/stable", private=False)
+            #self.options["gtest"].shared = self.options.shared
+            self.options["gtest"].shared = False
+        else:
+            del self.requires["gtest"]
+
+        #TODO if building test, add gtest!
 
     def build(self):
         cmake = CMake(self.settings)
         build_tests = "-DBUILD_CPR_TESTS=OFF" if not self.options.build_cpr_tests else ""
         insecure_curl = "-DINSECURE_CURL=ON" if self.options.insecure_curl else ""
         generate_coverage = "-DGENERATE_COVERAGE=ON" if self.options.generate_coverage else ""
-        use_openssl = "-DCMAKE_USE_OPENSSL=OFF" if not self.options.use_openssl else ""
 
-        self.run('cmake -DUSE_SYSTEM_CURL=ON -DCONAN=YES %s %s %s %s "%s/cpr" %s' % (build_tests, insecure_curl, generate_coverage, use_openssl, self.conanfile_directory, cmake.command_line))
+        self.run('cmake -DUSE_SYSTEM_CURL=ON %s %s %s "%s/cpr" %s' % (build_tests, insecure_curl, generate_coverage, self.conanfile_directory, cmake.command_line))
         self.run('cmake --build . %s' % cmake.build_config)
 
     def package(self):
