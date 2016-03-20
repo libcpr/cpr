8b767e1704b181c03071d58c504702b66cce811c
Felix Vanorder <Felix.Vanorder@gmail.com>
2016-03-20 23:32:54 +0000

    Feed to curl_easy_setopt appropriate typed timeout.

--- cpr/session.cpp
+++ cpr/session.cpp
@@ -126,7 +126,8 @@ void Session::Impl::SetHeader(const Header& header) {
 void Session::Impl::SetTimeout(const Timeout& timeout) {
     auto curl = curl_->handle;
     if (curl) {
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, timeout.ms.count());
+        long milliseconds = timeout.ms.count();
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT_MS, milliseconds);
     }
 }
 
