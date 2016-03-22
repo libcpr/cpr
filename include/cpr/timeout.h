457459df300b2c65084cac758b8dc28538da8c06
Felix Vanorder <Felix.Vanorder@gmail.com>
2016-03-22 16:24:48 +0000

    Check for under/overflow in Timeout constructor. Codestyle fixes.

--- include/cpr/timeout.h
+++ include/cpr/timeout.h
@@ -3,13 +3,23 @@
 
 #include <cstdint>
 #include <chrono>
+#include <limits>
+#include <stdexcept>
 
 namespace cpr {
 
 class Timeout {
   public:
-    Timeout(const std::chrono::milliseconds& timeout) : ms(timeout) {}
-    Timeout(const std::int32_t& timeout) : ms(timeout) {}
+    Timeout(const std::chrono::milliseconds& duration) : ms{duration} {
+        if(ms.count() > std::numeric_limits<long>::max()) {
+            throw std::overflow_error("cpr::Timeout: timeout value overflow.");
+        }
+        if(ms.count() < std::numeric_limits<long>::min()) {
+            throw std::underflow_error("cpr::Timeout: timeout value underflow.");
+        }
+    }
+    Timeout(const std::int32_t& milliseconds)
+            : Timeout{std::chrono::milliseconds(milliseconds)} {}
 
     std::chrono::milliseconds ms;
 };
