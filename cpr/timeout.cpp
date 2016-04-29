b031bb69a9d170cf2aeb018ee1fcaa380a352ff1
Felix Vanorder <Felix.Vanorder@gmail.com>
2016-04-29 16:05:03 +0100

    Apply clang-format to cpr/timeout.cpp.

--- cpr/timeout.cpp
+++ cpr/timeout.cpp
@@ -8,15 +8,16 @@
 namespace cpr {
 
 long Timeout::Milliseconds() const {
-
     static_assert(std::is_same<std::chrono::milliseconds, decltype(ms)>::value,
                   "Following casting expects milliseconds.");
 
-    if(ms.count() > std::numeric_limits<long>::max()) {
-        throw std::overflow_error("cpr::Timeout: timeout value overflow: " + std::to_string(ms.count()) + " ms.");
+    if (ms.count() > std::numeric_limits<long>::max()) {
+        throw std::overflow_error("cpr::Timeout: timeout value overflow: " +
+                                  std::to_string(ms.count()) + " ms.");
     }
-    if(ms.count() < std::numeric_limits<long>::min()) {
-        throw std::underflow_error("cpr::Timeout: timeout value underflow: " + std::to_string(ms.count()) + " ms.");
+    if (ms.count() < std::numeric_limits<long>::min()) {
+        throw std::underflow_error("cpr::Timeout: timeout value underflow: " +
+                                   std::to_string(ms.count()) + " ms.");
     }
 
     return static_cast<long>(ms.count());
