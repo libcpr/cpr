fe2b08dcbbc945be98a4c7c0b5f0ea80eae5561d
msly <msly.2008@163.com>
2017-02-18 00:22:21 +0800

    enable all supported built-in compressions

--- cpr/session.cpp
+++ cpr/session.cpp
@@ -69,6 +69,8 @@ Session::Impl::Impl() {
         curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
         curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, curl_->error);
         curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "");
+        /* enable all supported built-in compressions */
+        curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "");
 #ifdef CPR_CURL_NOSIGNAL
         curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
 #endif
