d5da1dc7659e88d5f0829c4392da86fea6e6903d
Rafael Ristovski <rafael.ristovski@gmail.com>
2017-06-23 22:13:31 +0200

    Fix padding of 'Error' and 'Response' classes.
    Triggers a warning (`-Wpadded`)

--- include/cpr/response.h
+++ include/cpr/response.h
@@ -24,13 +24,13 @@ class Response {
               url{CPR_FWD(p_url)}, elapsed{p_elapsed}, cookies{CPR_FWD(p_cookies)},
               error{CPR_FWD(p_error)} {}
 
-    std::int32_t status_code;
     std::string text;
     Header header;
     Url url;
     double elapsed;
     Cookies cookies;
     Error error;
+    std::int32_t status_code;
 };
 
 } // namespace cpr
