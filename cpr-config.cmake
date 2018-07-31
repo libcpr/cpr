8eb16e267843d18834bf42d9ae4ccfb58f34bdb5
Yuri <yuri@rawbw.com>
2018-07-30 20:48:47 -0700

    Install headers, cmake file.

--- cpr-config.cmake
+++ cpr-config.cmake
@@ -11,11 +11,15 @@
 #                     file that uses this interface
 
 find_path(CPR_INCLUDE_DIR
-          NAMES cpr.h)
+          NAMES cpr/cpr.h
+		  PATHS
+		  ${CMAKE_INSTALL_PREFIX}/include)
 
 find_library(CPR_LIBRARY
-             NAMES cpr
-             HINTS ${CPR_LIBRARY_ROOT})
+			 NAMES cpr
+			 PATHS
+			 ${CMAKE_INSTALL_PREFIX}/lib
+			 )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(CPR REQUIRED_VARS CPR_LIBRARY CPR_INCLUDE_DIR)
