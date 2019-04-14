803c85602af9ee7b8c6d83145f851cb4565ce3c7
Vasily Lobaskin <liastre@outlook.com>
2019-04-14 19:42:17 +0300

    Extend VerifySsl option params to allow set host and peer verification separately

--- cpr/ssl_options.cpp
+++ cpr/ssl_options.cpp
@@ -2,10 +2,19 @@
 
 namespace cpr {
 
-VerifySsl::VerifySsl(bool verify) : verify_{verify} {}
+VerifySsl::VerifySsl(bool verifyPeer, bool verifyHost)
+        : verifyPeer_{verifyPeer}, verifyHost_{verifyHost} {}
+
+bool VerifySsl::isVerifyPeerOn() const {
+    return verifyPeer_;
+}
+
+bool VerifySsl::isVerifyHostOn() const {
+    return verifyHost_;
+}
 
 VerifySsl::operator bool() const {
-    return verify_;
+    return verifyPeer_ && verifyHost_;
 }
 
 } // namespace cpr
--- include/cpr/ssl_options.h
+++ include/cpr/ssl_options.h
@@ -3,15 +3,30 @@
 
 namespace cpr {
 
+/**
+ * @brief SLL certificate verification option.
+ *
+ * Curl related options:
+ * - https://curl.haxx.se/libcurl/c/CURLOPT_SSL_VERIFYPEER.html
+ * - https://curl.haxx.se/libcurl/c/CURLOPT_SSL_VERIFYHOST.html
+ */
 class VerifySsl {
   public:
     VerifySsl() {}
-    VerifySsl(bool verify);
+    /**
+     * @param [in] verifyPeer - verify the authenticity of the peer's certificate
+     * @param [in] verifyHost - verify that the server certificate is for the server it is known as
+     */
+    VerifySsl(bool verifyPeer, bool verifyHost = true);
+
+    bool isVerifyPeerOn() const;
+    bool isVerifyHostOn() const;
 
     operator bool() const;
 
   private:
-    bool verify_ = true;
+    bool verifyPeer_{true};
+    bool verifyHost_{true};
 };
 
 } // namespace cpr
