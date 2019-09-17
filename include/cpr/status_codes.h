fa7ffb8958ab43e94b34691a69d1d39f234b5247
Diogo <diogo464@protonmail.com>
2019-09-17 11:50:14 +0000

    Remove status code number from variable name. Added helper functions

--- include/cpr/status_codes.h
+++ include/cpr/status_codes.h
@@ -3,69 +3,92 @@
 #include <cstdint>
 namespace cpr {
 namespace status {
-constexpr std::uint32_t HTTP_100_CONTINUE = 100;
-constexpr std::uint32_t HTTP_101_SWITCHING_PROTOCOL = 101;
-constexpr std::uint32_t HTTP_102_PROCESSING = 102;
-constexpr std::uint32_t HTTP_103_EARLY_HINTS = 103;
-constexpr std::uint32_t HTTP_200_OK = 200;
-constexpr std::uint32_t HTTP_201_CREATED = 201;
-constexpr std::uint32_t HTTP_202_ACCEPTED = 202;
-constexpr std::uint32_t HTTP_203_NON_AUTHORITATIVE_INFORMATION = 203;
-constexpr std::uint32_t HTTP_204_NO_CONTENT = 204;
-constexpr std::uint32_t HTTP_205_RESET_CONTENT = 205;
-constexpr std::uint32_t HTTP_206_PARTIAL_CONTENT = 206;
-constexpr std::uint32_t HTTP_207_MULTI_STATUS = 207;
-constexpr std::uint32_t HTTP_208_ALREADY_REPORTED = 208;
-constexpr std::uint32_t HTTP_226_IM_USED = 226;
-constexpr std::uint32_t HTTP_300_MULTIPLE_CHOICE = 300;
-constexpr std::uint32_t HTTP_301_MOVED_PERMANENTLY = 301;
-constexpr std::uint32_t HTTP_302_FOUND = 302;
-constexpr std::uint32_t HTTP_303_SEE_OTHER = 303;
-constexpr std::uint32_t HTTP_304_NOT_MODIFIED = 304;
-constexpr std::uint32_t HTTP_305_USE_PROXY = 305;
-constexpr std::uint32_t HTTP_306_UNUSED = 306;
-constexpr std::uint32_t HTTP_307_TEMPORARY_REDIRECT = 307;
-constexpr std::uint32_t HTTP_308_PERMANENT_REDIRECT = 308;
-constexpr std::uint32_t HTTP_400_BAD_REQUEST = 400;
-constexpr std::uint32_t HTTP_401_UNAUTHORIZED = 401;
-constexpr std::uint32_t HTTP_402_PAYMENT_REQUIRED = 402;
-constexpr std::uint32_t HTTP_403_FORBIDDEN = 403;
-constexpr std::uint32_t HTTP_404_NOT_FOUND = 404;
-constexpr std::uint32_t HTTP_405_METHOD_NOT_ALLOWED = 405;
-constexpr std::uint32_t HTTP_406_NOT_ACCEPTABLE = 406;
-constexpr std::uint32_t HTTP_407_PROXY_AUTHENTICATION_REQUIRED = 407;
-constexpr std::uint32_t HTTP_408_REQUEST_TIMEOUT = 408;
-constexpr std::uint32_t HTTP_409_CONFLICT = 409;
-constexpr std::uint32_t HTTP_410_GONE = 410;
-constexpr std::uint32_t HTTP_411_LENGTH_REQUIRED = 411;
-constexpr std::uint32_t HTTP_412_PRECONDITION_FAILED = 412;
-constexpr std::uint32_t HTTP_413_PAYLOAD_TOO_LARGE = 413;
-constexpr std::uint32_t HTTP_414_URI_TOO_LONG = 414;
-constexpr std::uint32_t HTTP_415_UNSUPPORTED_MEDIA_TYPE = 415;
-constexpr std::uint32_t HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
-constexpr std::uint32_t HTTP_417_EXPECTATION_FAILED = 417;
-constexpr std::uint32_t HTTP_418_IM_A_TEAPOT = 418;
-constexpr std::uint32_t HTTP_421_MISDIRECTED_REQUEST = 421;
-constexpr std::uint32_t HTTP_422_UNPROCESSABLE_ENTITY = 422;
-constexpr std::uint32_t HTTP_423_LOCKED = 423;
-constexpr std::uint32_t HTTP_424_FAILED_DEPENDENCY = 424;
-constexpr std::uint32_t HTTP_425_TOO_EARLY = 425;
-constexpr std::uint32_t HTTP_426_UPGRADE_REQUIRED = 426;
-constexpr std::uint32_t HTTP_428_PRECONDITION_REQUIRED = 428;
-constexpr std::uint32_t HTTP_429_TOO_MANY_REQUESTS = 429;
-constexpr std::uint32_t HTTP_431_REQUEST_HEADER_FIELDS_TOO_LARGE = 431;
-constexpr std::uint32_t HTTP_451_UNAVAILABLE_FOR_LEGAL_REASONS = 451;
-constexpr std::uint32_t HTTP_500_INTERNAL_SERVER_ERROR = 500;
-constexpr std::uint32_t HTTP_501_NOT_IMPLEMENTED = 501;
-constexpr std::uint32_t HTTP_502_BAD_GATEWAY = 502;
-constexpr std::uint32_t HTTP_503_SERVICE_UNAVAILABLE = 503;
-constexpr std::uint32_t HTTP_504_GATEWAY_TIMEOUT = 504;
-constexpr std::uint32_t HTTP_505_HTTP_VERSION_NOT_SUPPORTED = 505;
-constexpr std::uint32_t HTTP_506_VARIANT_ALSO_NEGOTIATES = 506;
-constexpr std::uint32_t HTTP_507_INSUFFICIENT_STORAGE = 507;
-constexpr std::uint32_t HTTP_508_LOOP_DETECTED = 508;
-constexpr std::uint32_t HTTP_510_NOT_EXTENDED = 510;
-constexpr std::uint32_t HTTP_511_NETWORK_AUTHENTICATION_REQUIRED = 511;
+// Information responses
+constexpr std::int32_t HTTP_CONTINUE = 100;
+constexpr std::int32_t HTTP_SWITCHING_PROTOCOL = 101;
+constexpr std::int32_t HTTP_PROCESSING = 102;
+constexpr std::int32_t HTTP_EARLY_HINTS = 103;
+// Sucessful responses
+constexpr std::int32_t HTTP_OK = 200;
+constexpr std::int32_t HTTP_CREATED = 201;
+constexpr std::int32_t HTTP_ACCEPTED = 202;
+constexpr std::int32_t HTTP_NON_AUTHORITATIVE_INFORMATION = 203;
+constexpr std::int32_t HTTP_NO_CONTENT = 204;
+constexpr std::int32_t HTTP_RESET_CONTENT = 205;
+constexpr std::int32_t HTTP_PARTIAL_CONTENT = 206;
+constexpr std::int32_t HTTP_MULTI_STATUS = 207;
+constexpr std::int32_t HTTP_ALREADY_REPORTED = 208;
+constexpr std::int32_t HTTP_IM_USED = 226;
+// Redirection messages
+constexpr std::int32_t HTTP_MULTIPLE_CHOICE = 300;
+constexpr std::int32_t HTTP_MOVED_PERMANENTLY = 301;
+constexpr std::int32_t HTTP_FOUND = 302;
+constexpr std::int32_t HTTP_SEE_OTHER = 303;
+constexpr std::int32_t HTTP_NOT_MODIFIED = 304;
+constexpr std::int32_t HTTP_USE_PROXY = 305;
+constexpr std::int32_t HTTP_UNUSED = 306;
+constexpr std::int32_t HTTP_TEMPORARY_REDIRECT = 307;
+constexpr std::int32_t HTTP_PERMANENT_REDIRECT = 308;
+// Client error responses
+constexpr std::int32_t HTTP_BAD_REQUEST = 400;
+constexpr std::int32_t HTTP_UNAUTHORIZED = 401;
+constexpr std::int32_t HTTP_PAYMENT_REQUIRED = 402;
+constexpr std::int32_t HTTP_FORBIDDEN = 403;
+constexpr std::int32_t HTTP_NOT_FOUND = 404;
+constexpr std::int32_t HTTP_METHOD_NOT_ALLOWED = 405;
+constexpr std::int32_t HTTP_NOT_ACCEPTABLE = 406;
+constexpr std::int32_t HTTP_PROXY_AUTHENTICATION_REQUIRED = 407;
+constexpr std::int32_t HTTP_REQUEST_TIMEOUT = 408;
+constexpr std::int32_t HTTP_CONFLICT = 409;
+constexpr std::int32_t HTTP_GONE = 410;
+constexpr std::int32_t HTTP_LENGTH_REQUIRED = 411;
+constexpr std::int32_t HTTP_PRECONDITION_FAILED = 412;
+constexpr std::int32_t HTTP_PAYLOAD_TOO_LARGE = 413;
+constexpr std::int32_t HTTP_URI_TOO_LONG = 414;
+constexpr std::int32_t HTTP_UNSUPPORTED_MEDIA_TYPE = 415;
+constexpr std::int32_t HTTP_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
+constexpr std::int32_t HTTP_EXPECTATION_FAILED = 417;
+constexpr std::int32_t HTTP_IM_A_TEAPOT = 418;
+constexpr std::int32_t HTTP_MISDIRECTED_REQUEST = 421;
+constexpr std::int32_t HTTP_UNPROCESSABLE_ENTITY = 422;
+constexpr std::int32_t HTTP_LOCKED = 423;
+constexpr std::int32_t HTTP_FAILED_DEPENDENCY = 424;
+constexpr std::int32_t HTTP_TOO_EARLY = 425;
+constexpr std::int32_t HTTP_UPGRADE_REQUIRED = 426;
+constexpr std::int32_t HTTP_PRECONDITION_REQUIRED = 428;
+constexpr std::int32_t HTTP_TOO_MANY_REQUESTS = 429;
+constexpr std::int32_t HTTP_REQUEST_HEADER_FIELDS_TOO_LARGE = 431;
+constexpr std::int32_t HTTP_UNAVAILABLE_FOR_LEGAL_REASONS = 451;
+// Server response errors
+constexpr std::int32_t HTTP_INTERNAL_SERVER_ERROR = 500;
+constexpr std::int32_t HTTP_NOT_IMPLEMENTED = 501;
+constexpr std::int32_t HTTP_BAD_GATEWAY = 502;
+constexpr std::int32_t HTTP_SERVICE_UNAVAILABLE = 503;
+constexpr std::int32_t HTTP_GATEWAY_TIMEOUT = 504;
+constexpr std::int32_t HTTP_HTTP_VERSION_NOT_SUPPORTED = 505;
+constexpr std::int32_t HTTP_VARIANT_ALSO_NEGOTIATES = 506;
+constexpr std::int32_t HTTP_INSUFFICIENT_STORAGE = 507;
+constexpr std::int32_t HTTP_LOOP_DETECTED = 508;
+constexpr std::int32_t HTTP_NOT_EXTENDED = 510;
+constexpr std::int32_t HTTP_NETWORK_AUTHENTICATION_REQUIRED = 511;
+
+constexpr bool is_informational(const std::int32_t code) {
+    return (code >= 100 && code < 200);
+}
+constexpr bool is_success(const std::int32_t code) {
+    return (code >= 200 && code < 300);
+}
+constexpr bool is_redirect(const std::int32_t code) {
+    return (code >= 300 && code < 400);
+}
+constexpr bool is_client_error(const std::int32_t code) {
+    return (code >= 400 && code < 500);
+}
+
+constexpr bool is_server_error(const std::int32_t code) {
+    return (code >= 500 && code < 600);
+}
+
 } // namespace status
 } // namespace cpr
 #endif
