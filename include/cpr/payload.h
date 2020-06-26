d98ec3b1e9f7b6beeed4b8e5f6755bbf912ed316
Dilshod Mukhtarov <dilshodm@gmail.com>
2020-06-26 16:45:08 +0400

    Broken API: can't use Payload{{my_string_key, my_string_value}}
    The Pair constructor accepts only r-value references which forces to use string variables as:
    Payload{{my_string_key.c_str(), my_string_value.c_str()}}.
    Added const reference to strings constructor

--- include/cpr/payload.h
+++ include/cpr/payload.h
@@ -12,6 +12,8 @@
 namespace cpr {
 
 struct Pair {
+    Pair(const std::string& p_key, const std::string& p_value)
+            : key(p_key), value(p_value) {}
     Pair(std::string&& p_key, std::string&& p_value)
             : key(std::move(p_key)), value(std::move(p_value)) {}
 
