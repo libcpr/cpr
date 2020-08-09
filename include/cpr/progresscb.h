748ac5324817664f68e5523e8dbf333260585719
ITotalJustice <47043333+ITotalJustice@users.noreply.github.com>
2020-08-09 10:49:57 +0100

    use wrapper cpr_off_t. fix * placement

--- include/cpr/progresscb.h
+++ include/cpr/progresscb.h
@@ -1,26 +1,28 @@
 #ifndef CPR_PROGRESSCB_H
 #define CPR_PROGRESSCB_H
 
+#include "cprtypes.h"
+
 namespace cpr {
 
 class ProgressCallback {
   #if LIBCURL_VERSION_NUM < 0x072000 // older than 7.32.0 uses the older callback.
   typedef int(*progress_cb_t)(void*, double, double, double, double);
   #else
-  typedef int(*progress_cb_t)(void*, int64_t, int64_t, int64_t, int64_t);
+  typedef int(*progress_cb_t)(void*, cpr_off_t, cpr_off_t, cpr_off_t, cpr_off_t);
   #endif
 
   public:
     ProgressCallback() = default;
     ProgressCallback(const progress_cb_t cb) : cb{cb} {}
-    ProgressCallback(const progress_cb_t cb, void * userpt)
+    ProgressCallback(const progress_cb_t cb, void* userpt)
           : cb{cb}, userpt{userpt} {}
 
     void AddCallback(const progress_cb_t callback) { cb = callback; }
-    void AddUserData(void *userData) { userpt = userData; }
+    void AddUserData(void* userData) { userpt = userData; }
 
     progress_cb_t cb;
-    void *userpt = nullptr;
+    void* userpt = nullptr;
 };
 
 } // namespace cpr
