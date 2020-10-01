cd78ad2f62981ca0aec6dac2840ced571f098c41
Fabian Sauter <sauter.fabian@mailbox.org>
2020-10-01 14:37:28 +0200

    Converted the CurlHolder to a shared_ptr

--- include/cpr/response.h
+++ include/cpr/response.h
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <cstdint>
 #include <curl/curl.h>
+#include <memory>
 #include <string>
 
 #include "cpr/cookies.h"
@@ -15,23 +16,10 @@
 namespace cpr {
 
 class Response {
-  public:
-    Response() = default;
-    Response(CURL* curl, std::string&& p_text, std::string&& p_header_string,
-             Cookies&& p_cookies = Cookies{}, Error&& p_error = Error{})
-            : text(std::move(p_text)), cookies(std::move(p_cookies)), error(std::move(p_error)) {
-        header = cpr::util::parseHeader(p_header_string, &status_line, &reason);
-        assert(curl);
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &status_code);
-        curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
-        char* url_string{nullptr};
-        curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url_string);
-        url = Url(url_string);
-        curl_easy_getinfo(curl, CURLINFO_SIZE_DOWNLOAD_T, &downloaded_bytes);
-        curl_easy_getinfo(curl, CURLINFO_SIZE_UPLOAD_T, &uploaded_bytes);
-        curl_easy_getinfo(curl, CURLINFO_REDIRECT_COUNT, &redirect_count);
-    }
+  private:
+    std::shared_ptr<CurlHolder> curl_;
 
+  public:
     long status_code;
     std::string text;
     Header header;
@@ -45,8 +33,25 @@ class Response {
     cpr_off_t uploaded_bytes;
     cpr_off_t downloaded_bytes;
     long redirect_count;
-};
 
+  public:
+    Response() = default;
+    Response(std::shared_ptr<CurlHolder> curl, std::string&& p_text, std::string&& p_header_string,
+             Cookies&& p_cookies = Cookies{}, Error&& p_error = Error{})
+            : curl_(std::move(curl)), text(std::move(p_text)), cookies(std::move(p_cookies)),
+              error(std::move(p_error)) {
+        header = cpr::util::parseHeader(p_header_string, &status_line, &reason);
+        assert(curl_->handle);
+        curl_easy_getinfo(curl_->handle, CURLINFO_RESPONSE_CODE, &status_code);
+        curl_easy_getinfo(curl_->handle, CURLINFO_TOTAL_TIME, &elapsed);
+        char* url_string{nullptr};
+        curl_easy_getinfo(curl_->handle, CURLINFO_EFFECTIVE_URL, &url_string);
+        url = Url(url_string);
+        curl_easy_getinfo(curl_->handle, CURLINFO_SIZE_DOWNLOAD_T, &downloaded_bytes);
+        curl_easy_getinfo(curl_->handle, CURLINFO_SIZE_UPLOAD_T, &uploaded_bytes);
+        curl_easy_getinfo(curl_->handle, CURLINFO_REDIRECT_COUNT, &redirect_count);
+    }
+};
 } // namespace cpr
 
 #endif
