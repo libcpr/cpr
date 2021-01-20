1080116b1ead298120c84f22913e6860bcd2b776
Fabian Sauter <sauter.fabian@mailbox.org>
2021-01-20 10:47:32 +0100

    Added support for  GCC10 static analysis

---
+++ cmake/gcc_analyze.cmake
@@ -0,0 +1,12 @@
+include(CheckCXXCompilerFlag)
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    check_cxx_compiler_flag("-fanalyzer" HAS_GCC_STATIC_ANALYZER)
+    if(HAS_GCC_STATIC_ANALYZER AND NOT ENABLE_LINTING)
+        option(ENABLE_GCC_STATIC_ANALYZER "Enable linting with the GCC static analyzer" OFF)
+        if(ENABLE_GCC_STATIC_ANALYZER)
+            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fanalyzer")
+            set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} -fanalyzer")
+        endif()
+    endif()
+endif()
