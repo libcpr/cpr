deb24436d153ebbfe0c6853ec839fe70edcbe034
Fabian Sauter <sauter.fabian@mailbox.org>
2021-04-26 20:44:10 +0200

    Added a first version for clang tidy integration

---
+++ cmake/clang-tidy.cmake
@@ -0,0 +1,13 @@
+if (NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+    find_program(CLANG_TIDY_EXECUTABLE NAMES clang-tidy-7 clang-tidy)
+    mark_as_advanced(CLANG_TIDY_EXECUTABLE)
+
+    if (${CLANG_TIDY_EXECUTABLE})
+        message(FATAL_ERROR "Clang-tidy not found")
+    else()
+        message(STATUS "Enabling clang-tidy")
+        set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_EXECUTABLE};-warnings-as-errors=*")
+    endif()
+else()
+    message(FATAL_ERROR "Clang-tidy is not supported when building for windows")
+endif()
---
+++ cmake/clear_variable.cmake
@@ -0,0 +1,11 @@
+macro(clear_variable)
+    cmake_parse_arguments(CLEAR_VAR "" "DESTINATION;BACKUP;REPLACE" "" ${ARGN})
+    set(${CLEAR_VAR_BACKUP} ${${CLEAR_VAR_DESTINATION}})
+    set(${CLEAR_VAR_DESTINATION} ${CLEAR_VAR_REPLACE})
+endmacro()
+
+macro(restore_variable)
+    cmake_parse_arguments(CLEAR_VAR "" "DESTINATION;BACKUP" "" ${ARGN})
+    set(${CLEAR_VAR_DESTINATION} ${${CLEAR_VAR_BACKUP}})
+    unset(${CLEAR_VAR_BACKUP})
+endmacro()
