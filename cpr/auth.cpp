5bccfc8763756ccecb66c11edf1bff54cc4a64e6
Simon Berger <46813094+simon-berger@users.noreply.github.com>
2022-05-04 09:40:19 +0200

    Unified basic, digest and ntlm authentication into one authenticaton class (#735)
    * Merged basic, digest and ntlm authentication
    
    * Removed old digest and ntlm authentication
    
    * Updated basic auth in get example of Readme
    
    * Adapted tests to merged authentication
    
    * Added cpr namespace of AuthMode in readme
    
    Co-authored-by: Fabian Sauter <sauter.fabian@mailbox.org>
    
    * Removed unnecessary keyword: virtual
    
    Co-authored-by: Fabian Sauter <sauter.fabian@mailbox.org>
    
    * Removed unnecessary keyword: virtual
    
    Co-authored-by: Fabian Sauter <sauter.fabian@mailbox.org>
    
    * Removed unnecessary keyword: virtual
    
    Co-authored-by: Fabian Sauter <sauter.fabian@mailbox.org>
    
    * Changed local variables to private
    
    Co-authored-by: Fabian Sauter <sauter.fabian@mailbox.org>
    
    Co-authored-by: Simon Berger <ge25fed@mytum.de>
    Co-authored-by: Fabian Sauter <sauter.fabian@mailbox.org>

--- cpr/auth.cpp
+++ cpr/auth.cpp
@@ -4,4 +4,8 @@ namespace cpr {
 const char* Authentication::GetAuthString() const noexcept {
     return auth_string_.c_str();
 }
+
+AuthMode Authentication::GetAuthMode() const noexcept {
+    return auth_mode_;
+}
 } // namespace cpr
