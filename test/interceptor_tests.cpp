32c169e1e9b81f83e1ead6dd3b1129acb76d9404
Simon Berger <ge25fed@mytum.de>
2022-05-16 16:49:56 +0200

    Added basic test for interceptor

---
+++ test/interceptor_tests.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+#include "cpr/cpr.h"
+#include "httpServer.hpp"
+
+using namespace cpr;
+
+static HttpServer* server = new HttpServer();
+
+class HiddenHelloWorldRedirectInterceptor : public Interceptor {
+  public:
+    Response intercept(Chain chain) {
+        // Get current session
+        Session* session = chain.session();
+
+        // Save original url
+        Url old_url = session->GetFullRequestUrl();
+
+        // Rewrite the url
+        Url url{server->GetBaseUrl() + "/hello.html"};
+        session->SetUrl(url);
+
+        // Procceed the chain
+        Response response = chain.proceed();
+
+        // Restore the url again
+        response.url = old_url;
+        return response;
+    };
+};
+
+TEST(InterceptorTest, HiddenUrlRewriteInterceptor) {
+    Url url{server->GetBaseUrl() + "/basic.json"};
+    Session session;
+    session.SetUrl(url);
+    session.AddInterceptor(std::make_shared<HiddenHelloWorldRedirectInterceptor>());
+    Response response = session.Get();
+
+    std::string expected_text{"Hello world!"};
+    EXPECT_EQ(expected_text, response.text);
+    EXPECT_EQ(url, response.url);
+    EXPECT_EQ(ErrorCode::OK, response.error.code);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::AddGlobalTestEnvironment(server);
+    return RUN_ALL_TESTS();
+}
