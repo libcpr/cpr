21d38109694772ce5525e50f633978d335b25413
saendigPhilip <philip.saendig@web.de>
2022-05-30 22:59:28 +0200

    Debian files and scripts for debian package build
    The library is packaged with the help of debmake and debuild.
    This commit also updates the CI script to build packages on a debian
    docker image with the help of the debmake and debuild packages.
    
    The images that come out of this script are still not perfect!
    lintian throws warnings and information messages that are still to be
    investigated and solved.
    
    To be tested.

---
+++ package-build/build-package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SRC_DIR=$1
+
+LIB='libcpr'
+
+LIB_DIR="$LIB-$VERSION"
+DEBIAN_DIR="$LIB_DIR/debian"
+
+ARCHIVE_NAME="$LIB_DIR.tar.gz"
+tar -czf --exclude-vcs $ARCHIVE_NAME $SRC_DIR
+tar -xzf $ARCHIVE_NAME
+
+cd $LIB_DIR
+
+debmake -e 'philip.saendig@gmail.com' -f 'Philip Saendig'
+
+cp -r package-build/debian-libcpr/* debian/
+
+debuild
+
+
