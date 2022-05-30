77709b3102039821b7fcae4df77b66c3ded1ce96
Simon Berger <simon.c.berger@gmail.com>
2022-05-30 09:00:57 +0200

    Rename function and add comments

--- cpr/ssl_ctx.cpp
+++ cpr/ssl_ctx.cpp
@@ -1,4 +1,5 @@
 #if defined OPENSSL
+
 #include "cpr/ssl_ctx.h"
 #include <openssl/err.h>
 #include <openssl/safestack.h>
@@ -6,37 +7,58 @@
 
 namespace cpr {
 
-// Implementation from: https://curl.se/libcurl/c/CURLOPT_SSL_CTX_DATA.html
-CURLcode sslctx_function(CURL* /*curl*/, void* sslctx, void* parm) {
+/**
+ * The ssl_ctx parameter is actually a pointer to the SSL library's SSL_CTX for OpenSSL.
+ * If an error is returned from the callback no attempt to establish a connection is made and
+ * the perform operation will return the callback's error code.
+ *
+ * Sources: https://curl.se/libcurl/c/CURLOPT_SSL_CTX_FUNCTION.html
+ *          https://curl.se/libcurl/c/CURLOPT_SSL_CTX_DATA.html
+ */
+CURLcode sslctx_function_load_ca_cert_from_buffer(CURL* /*curl*/, void* sslctx, void* raw_cert_buf) {
+    // Check arguments
+    if (raw_cert_buf == nullptr || sslctx == nullptr) {
+        printf("Invalid callback arguments\n");
+        return CURLE_ABORTED_BY_CALLBACK;
+    }
+
+    // Setup pointer
     X509_STORE* store = nullptr;
     X509* cert = nullptr;
     BIO* bio = nullptr;
-    char* mypem = static_cast<char*>(parm);
-    /* get a BIO */
-    bio = BIO_new_mem_buf(mypem, -1);
-    /* use it to read the PEM formatted certificate from memory into an
-     * X509 structure that SSL can use
-     */
+    char* cert_buf = static_cast<char*>(raw_cert_buf);
+
+    // Create a memory BIO using the data of cert_buf.
+    // Note: It is assumed, that cert_buf is nul terminated and its length is determined by strlen.
+    bio = BIO_new_mem_buf(cert_buf, -1);
+
+    // Load the PEM formatted certicifate into an X509 structure which OpenSSL can use.
     PEM_read_bio_X509(bio, &cert, nullptr, nullptr);
     if (cert == nullptr) {
-        printf("PEM_read_bio_X509 failed...\n");
+        printf("PEM_read_bio_X509 failed\n");
+        return CURLE_ABORTED_BY_CALLBACK;
     }
 
-    /* get a pointer to the X509 certificate store (which may be empty) */
+    // Get a pointer to the current certificate verification storage
     store = SSL_CTX_get_cert_store(static_cast<SSL_CTX*>(sslctx));
 
-    /* add our certificate to this store */
-    if (X509_STORE_add_cert(store, cert) == 0) {
-        printf("error adding certificate\n");
+    // Add the loaded certificate to the verification storage
+    int status = X509_STORE_add_cert(store, cert);
+    if (status == 0) {
+        printf("Error adding certificate\n");
+        return CURLE_ABORTED_BY_CALLBACK;
     }
 
-    /* decrease reference counts */
+    // Decrement the reference count of the X509 structure cert and frees it up
     X509_free(cert);
+
+    // Free the entire bio chain
     BIO_free(bio);
 
-    /* all set to go */
+    // The CA certificate was loaded successfully into the verification storage
     return CURLE_OK;
 }
 
 } // namespace cpr
+
 #endif
--- include/cpr/ssl_ctx.h
+++ include/cpr/ssl_ctx.h
@@ -2,11 +2,21 @@
 #define CPR_SSL_CTX_H
 
 #if defined OPENSSL
+
 #include <curl/curl.h>
 
 namespace cpr {
 
-CURLcode sslctx_function(CURL* curl, void* sslctx, void* parm);
+/**
+ * This callback function loads a CA certificate from raw_cert_buf and gets called by libcurl
+ * just before the initialization of an SSL connection.
+ * The raw_cert_buf argument is set with the CURLOPT_SSL_CTX_DATA option and has to be a nul
+ * terminated buffer.
+ *
+ * Sources: https://curl.se/libcurl/c/CURLOPT_SSL_CTX_FUNCTION.html
+ *         https://curl.se/libcurl/c/CURLOPT_SSL_CTX_DATA.html
+ */
+CURLcode sslctx_function_load_ca_cert_from_buffer(CURL* curl, void* sslctx, void* raw_cert_buf);
 
 } // Namespace cpr
 #endif
