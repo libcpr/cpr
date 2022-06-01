ca839ea84c7dcde1deb9cc74de7e86518415c9ce
Simon Berger <simon.c.berger@gmail.com>
2022-06-01 16:51:04 +0200

    Add UploadRange assertions and fix getFileSitze

--- include/cpr/range.h
+++ include/cpr/range.h
@@ -1,8 +1,11 @@
 #ifndef CPR_RANGE_H
 #define CPR_RANGE_H
 
+#include <cassert>
 #include <cstdint>
 #include <curl/curl.h>
+
+#define assertm(exp, msg) assert(((void) msg, exp))
 
 namespace cpr {
 
@@ -47,15 +50,24 @@ class MultiRange {
 
 class UploadRange : private Range {
   public:
-    UploadRange(const std::int64_t p_resume_from, const std::int64_t p_finish_at) : Range(p_resume_from, p_finish_at) {}
+    UploadRange(const std::int64_t p_resume_from, const std::int64_t p_finish_at) : Range(p_resume_from, p_finish_at) {
+        // Upload ranges do not support negative values / partial ranges
+        assertm(p_resume_from >= 0, "UploadRange does not support negative values / partial ranges");
+        assertm(p_finish_at >= 0, "UploadRange does not support negative values / partial ranges");
+        assertm(p_resume_from <= p_finish_at, "p_resume_from has to be smaller equal than p_resume_from");
+    }
 
     curl_off_t getResumeFrom() const {
         return static_cast<curl_off_t>(this->Range::getResumeFrom());
     }
 
     curl_off_t getFilesize() const {
-        return static_cast<curl_off_t>(this->Range::getFinishAt()) - this->getResumeFrom();
-        ;
+        // More information: https://curl.se/mail/lib-2019-05/0012.html
+        // filesize = resume_from + part size =
+        //          = resume_from + (finish_at - resume_from + 1)
+        //          = finish_at + 1
+        // Note: We assume that the actual filesize is larger than or equal to finish_at
+        return static_cast<curl_off_t>(this->Range::getFinishAt()) + 1;
     }
 };
 
