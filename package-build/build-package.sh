ed05f3e5e8763bb0b01f4ea39e1ae476b536a216
saendigPhilip <philip.saendig@web.de>
2022-06-06 16:07:30 +0200

    Fixed package build
    After a lot of testing with local docker images, the building process
    should be ready to run on a github worker.
    The lintian warning concerning the existing symlinks can (as of now) be
    ignored, as they turn into an error if the symlinks don't exist at all

--- package-build/build-package.sh
+++ package-build/build-package.sh
@@ -9,6 +9,7 @@ DEBIAN_DIR="$LIB_DIR/debian"
 
 ARCHIVE_NAME="$LIB_DIR.tar.gz"
 
+echo -e "Preparing tar archive and directory\n"
 cp -r $SRC_DIR $LIB_DIR
 
 tar --exclude-vcs -czf $ARCHIVE_NAME $LIB_DIR
@@ -16,10 +17,13 @@ tar -xzf $ARCHIVE_NAME
 
 cd $LIB_DIR
 
+echo -e "\n\nCalling debmake\n"
 debmake -e 'philip.saendig@gmail.com' -f 'Philip Saendig'
 
+echo -e "\n\nCopying prepared debian files to directory\n"
 cp -r package-build/debian-libcpr/* debian/
 
+echo -e "\n\nCalling debuild\n"
 debuild
 
 
