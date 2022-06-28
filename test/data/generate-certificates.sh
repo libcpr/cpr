6e3d8c5448aa56237d2d12c941a7fd7ce127d08e
saendigPhilip <philip.saendig@web.de>
2022-06-28 22:45:39 +0200

    New certificates with ECC and longer validity
    Certificates have a validity of 5 years by default now.
    New root, server, and client certificates that can be used in tests are
    now available.
    The certificates now use the state-of-the-art algorithm ed25519 for
    asymmetric crypto.

--- test/data/generate-certificates.sh
+++ test/data/generate-certificates.sh
@@ -16,23 +16,50 @@ mkdir -p $KEY_PATH $CRT_PATH $CA_PATH/db $CA_PATH/private $CA_PATH/certificates
 touch $CA_PATH/db/index
 openssl rand -hex 16  > $CA_PATH/db/serial
 
+# Generate all keys
+openssl genpkey -algorithm ed25519 -out $KEY_PATH/root-ca.key
+openssl genpkey -algorithm ed25519 -out $KEY_PATH/server.key
+openssl genpkey -algorithm ed25519 -out $KEY_PATH/client.key
+
+
 
 # Generate a Certificate Signing Request for the Root CA based on a config file
-openssl req -new -config root-ca.cnf -out root-ca.csr -keyout $KEY_PATH/root-ca.key -passout pass:
+openssl req -new \
+    -config root-ca.cnf -out root-ca.csr \
+    -key $KEY_PATH/root-ca.key
 
-# Self-Sign the root certificate
-openssl ca -batch -selfsign -config root-ca.cnf -in root-ca.csr -out $CRT_PATH/root-ca.crt -extensions ca_ext -passin pass:
+# Self-sign the root certificate
+openssl ca -batch \
+    -selfsign -config root-ca.cnf \
+    -extensions ca_ext \
+    -in root-ca.csr -out $CRT_PATH/root-ca.crt
 
 
-# Create a Certificate Signing request for a server certificate
-openssl req -new -nodes -config server.cnf -out server.csr -keyout $KEY_PATH/server.key -passout pass:
+# Create a Certificate Signing request for the server certificate
+openssl req -new \
+    -config server.cnf -out server.csr \
+    -key $KEY_PATH/server.key
+
 # Issue the server certificate
-openssl ca -batch -config root-ca.cnf -in server.csr -out $CRT_PATH/server.crt -extensions server_ext -passin pass:
+openssl ca -batch \
+    -config root-ca.cnf \
+    -extensions server_ext \
+    -in server.csr -out $CRT_PATH/server.crt \
+    -days 1825
 
-# Create a Certificate Signing request for a client certificate
-openssl req -new -config client.cnf -out client.csr -keyout $KEY_PATH/client.key -passout pass:
+
+# Create a Certificate Signing request for the client certificate
+openssl req -new \
+    -config client.cnf -out client.csr \
+    -key $KEY_PATH/client.key
+
 # Issue the client certificate
-openssl ca -batch -config root-ca.cnf -in client.csr -out $CRT_PATH/client.crt -extensions client_ext -passin pass:
+openssl ca -batch \
+    -config root-ca.cnf \
+    -extensions client_ext \
+    -in client.csr -out $CRT_PATH/client.crt \
+    -days 1825
+
 
 
 # Clean up
