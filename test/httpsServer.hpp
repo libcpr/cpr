462cd7065aae71e007b679f71a75a5576556079d
saendigPhilip <philip.saendig@web.de>
2022-06-30 10:54:27 +0200

    Adapted HTTPS server for working with mongoose 7
    Changed method signatures, structure names, etc.
    The way, HTTPS connections are accepted in mongoose 7 differ from
    mongoose 6: On triggering of the accept event, the callback method needs
    to initialize the HTTPS connection

--- test/httpsServer.hpp
+++ test/httpsServer.hpp
@@ -14,6 +14,7 @@ class HttpsServer : public AbstractServer {
     const std::string baseDirPath;
     const std::string sslCertFileName;
     const std::string sslKeyFileName;
+    struct mg_tls_opts tlsOpts;
 
   public:
     explicit HttpsServer(std::string&& baseDirPath, std::string&& sslCertFileName, std::string&& sslKeyFileName);
@@ -22,16 +23,17 @@ class HttpsServer : public AbstractServer {
     std::string GetBaseUrl() override;
     uint16_t GetPort() override;
 
-    void OnRequest(mg_connection* conn, http_message* msg) override;
-    static void OnRequestHello(mg_connection* conn, http_message* msg);
-    static void OnRequestNotFound(mg_connection* conn, http_message* msg);
+    void OnRequest(mg_connection* conn, mg_http_message* msg) override;
+    static void OnRequestHello(mg_connection* conn, mg_http_message* msg);
+    static void OnRequestNotFound(mg_connection* conn, mg_http_message* msg);
 
     const std::string& getBaseDirPath() const;
     const std::string& getSslCertFileName() const;
     const std::string& getSslKeyFileName() const;
 
   protected:
-    mg_connection* initServer(mg_mgr* mgr, MG_CB(mg_event_handler_t event_handler, void* user_data)) override;
+    mg_connection* initServer(mg_mgr* mgr, mg_event_handler_t event_handler) override;
+    void acceptConnection(mg_connection* conn) override;
 };
 } // namespace cpr
 
