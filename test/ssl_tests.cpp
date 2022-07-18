a25c1ceadbded2bcf2bc6e86c17485d61b826eb7
Simon Berger <simon.c.berger@gmail.com>
2022-07-18 11:51:06 +0200

    Remove newline from Authority Key Identifier

--- test/ssl_tests.cpp
+++ test/ssl_tests.cpp
@@ -83,7 +83,7 @@ TEST(SslTests, GetCertInfos) {
                     "Public Key Algorithm:ED25519",
                     "X509v3 Subject Alternative Name:DNS:localhost, IP Address:127.0.0.1, IP Address:0:0:0:0:0:0:0:1",
                     "X509v3 Subject Key Identifier:39:C1:81:38:01:DC:55:38:E5:2F:4E:7A:D0:4C:84:7B:B7:27:D3:AF",
-                    "X509v3 Authority Key Identifier:E4:F2:F3:85:0E:B7:85:75:84:76:E3:43:D1:B6:9D:14:B8:E2:A4:B7\n",
+                    "X509v3 Authority Key Identifier:E4:F2:F3:85:0E:B7:85:75:84:76:E3:43:D1:B6:9D:14:B8:E2:A4:B7",
                     "Start date:Jun 29 11:33:07 2022 GMT",
                     "Expire date:Jun 28 11:33:07 2027 GMT",
                     "Signature:2e:0d:a1:0d:f5:90:77:e9:eb:84:7d:80:63:63:4d:8a:eb:d9:23:57:1f:21:2a:ed:81:b4:a8:58:b9:00:1b:cb:5c:90:1b:33:6b:f6:ec:42:20:63:54:d6:60:ee:37:14:1b:1c:95:0b:33:ea:67:29:d4:cc:d9:7e:34:fd:47:04:",
@@ -112,6 +112,12 @@ t4V1hHbjQ9G2nRS44qS3MAUGAytlcANBAC4NoQ31kHfp64R9gGNjTYrr2SNXHyEq
             std::size_t search_index = (*entry_it).find(search_string);
             if (search_index != std::string::npos) {
                 (*entry_it).replace(search_index, search_string.length(), "Identifier:");
+
+                search_string = "\n";
+                search_index = (*entry_it).find(search_string);
+                if (search_index != std::string::npos) {
+                    (*entry_it).replace(search_index, search_string.length(), "");
+                }
             }
             EXPECT_EQ(*expectedEntry_it, *entry_it);
         }
