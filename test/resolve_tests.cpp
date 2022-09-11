83dfffb5aa4ecdda0b1ef095ab1457851b978962
hexadec <andris.cseh@gmail.com>
2022-09-11 18:39:02 +0200

    Add test case (for PR#820)

---
+++ test/resolve_tests.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <string>
+
+#include "cpr/cpr.h"
+#include "httpServer.hpp"
+
+using namespace cpr;
+
+static HttpServer* server = new HttpServer();
+
+TEST(ResolveTests, HelloWorldTest) {
+    Url url{"http://www.example.com:" + std::to_string(server->GetPort()) + "/hello.html"};
+    Resolve resolve{"www.example.com", "127.0.0.1", {server->GetPort()}};
+    Response response = cpr::Get(url, resolve);
+    std::string expected_text{"Hello world!"};
+    EXPECT_EQ(expected_text, response.text);
+    EXPECT_EQ(url, response.url);
+    EXPECT_EQ(std::string{"text/html"}, response.header["content-type"]);
+    EXPECT_EQ(200, response.status_code);
+    EXPECT_EQ(ErrorCode::OK, response.error.code);
+}
+
+TEST(ResolveTests, RedirectToNotResponding) {
+    Url url{"http://www.example.com/hello.html"};
+    Resolve resolve{"www.example.com", "127.0.0.1", {80}};
+    Response response = cpr::Get(url, resolve);
+    EXPECT_EQ(0, response.status_code);
+    EXPECT_EQ(response.error.code == ErrorCode::OPERATION_TIMEDOUT || response.error.code == ErrorCode::CONNECTION_FAILURE, response.error.code);
+}
