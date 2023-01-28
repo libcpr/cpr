9450f8c0d002d133edf7747bb91faa43f725d26e
Christopher Peterson Sauer <christophersauer@pacbell.net>
2023-01-27 20:21:13 -0800

    Improvements to CPR_USE_BOOST_FILESYSTEM

--- include/cpr/filesystem.h
+++ include/cpr/filesystem.h
@@ -2,17 +2,16 @@
 #define CPR_FILESYSTEM_H
 
 // Include filesystem into the namespace "fs" from either "filesystem" or "experimental/filesystem" or "boost/filesystem"
+#ifdef CPR_USE_BOOST_FILESYSTEM
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
 // cppcheck-suppress preprocessorErrorDirective
-#if __has_include(<filesystem>)
+#elif __has_include(<filesystem>)
 #include <filesystem>
 namespace fs = std::filesystem;
 #elif __has_include("experimental/filesystem")
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
-// cppcheck-suppress preprocessorErrorDirective
-#elif defined(CPR_USE_BOOST_FILESYSTEM) && __has_include(<boost/filesystem.hpp>)
-#include <boost/filesystem.hpp>
-namespace fs = boost::filesystem;
 #else
 #error "Failed to include <filesystem> header!"
 #endif
