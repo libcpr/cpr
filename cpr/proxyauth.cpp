bcdb42ceeeca1ec65bd9e51f60a4add21b35c072
bleriaf <109803937+bleriaf@users.noreply.github.com>
2023-03-05 10:18:30 +0100

    Split Proxy Auth into Username and Password (#875)
    * dont use proxyauth_any
    
    * Update proxyauth.cpp
    
    * Fixed new ProxyAuth interface
    
    ---------
    
    Co-authored-by: Fabian Sauter <sauter.fabian@mailbox.org>

--- cpr/proxyauth.cpp
+++ cpr/proxyauth.cpp
@@ -3,19 +3,28 @@
 
 namespace cpr {
 EncodedAuthentication::~EncodedAuthentication() noexcept {
-    util::secureStringClear(auth_string_);
+    util::secureStringClear(username);
+    util::secureStringClear(password);
 }
 
-const char* EncodedAuthentication::GetAuthString() const noexcept {
-    return auth_string_.c_str();
+const std::string& EncodedAuthentication::GetUsername() const {
+    return username;
+}
+
+const std::string& EncodedAuthentication::GetPassword() const {
+    return password;
 }
 
 bool ProxyAuthentication::has(const std::string& protocol) const {
     return proxyAuth_.count(protocol) > 0;
 }
 
-const char* ProxyAuthentication::operator[](const std::string& protocol) {
-    return proxyAuth_[protocol].GetAuthString();
+const char* ProxyAuthentication::GetUsername(const std::string& protocol) {
+    return proxyAuth_[protocol].username.c_str();
+}
+
+const char* ProxyAuthentication::GetPassword(const std::string& protocol) {
+    return proxyAuth_[protocol].password.c_str();
 }
 
 } // namespace cpr
--- include/cpr/proxyauth.h
+++ include/cpr/proxyauth.h
@@ -3,15 +3,20 @@
 
 #include <initializer_list>
 #include <map>
+#include <string>
 
 #include "cpr/auth.h"
 #include "cpr/util.h"
 
 namespace cpr {
+class ProxyAuthentication;
+
 class EncodedAuthentication {
+    friend ProxyAuthentication;
+
   public:
-    EncodedAuthentication() : auth_string_{""} {}
-    EncodedAuthentication(std::string username, std::string password) : auth_string_{cpr::util::urlEncode(std::move(username)) + ":" + cpr::util::urlEncode(std::move(password))} {}
+    EncodedAuthentication() = default;
+    EncodedAuthentication(const std::string& p_username, const std::string& p_password) : username(util::urlEncode(p_username)), password(util::urlEncode(p_password)) {}
     EncodedAuthentication(const EncodedAuthentication& other) = default;
     EncodedAuthentication(EncodedAuthentication&& old) noexcept = default;
     virtual ~EncodedAuthentication() noexcept;
@@ -19,20 +24,23 @@ class EncodedAuthentication {
     EncodedAuthentication& operator=(EncodedAuthentication&& old) noexcept = default;
     EncodedAuthentication& operator=(const EncodedAuthentication& other) = default;
 
-    const char* GetAuthString() const noexcept;
+    [[nodiscard]] const std::string& GetUsername() const;
+    [[nodiscard]] const std::string& GetPassword() const;
 
-  protected:
-    std::string auth_string_;
+  private:
+    std::string username;
+    std::string password;
 };
 
 class ProxyAuthentication {
   public:
     ProxyAuthentication() = default;
     ProxyAuthentication(const std::initializer_list<std::pair<const std::string, EncodedAuthentication>>& auths) : proxyAuth_{auths} {}
-    ProxyAuthentication(const std::map<std::string, EncodedAuthentication>& auths) : proxyAuth_{auths} {}
+    explicit ProxyAuthentication(const std::map<std::string, EncodedAuthentication>& auths) : proxyAuth_{auths} {}
 
-    bool has(const std::string& protocol) const;
-    const char* operator[](const std::string& protocol);
+    [[nodiscard]] bool has(const std::string& protocol) const;
+    const char* GetUsername(const std::string& protocol);
+    const char* GetPassword(const std::string& protocol);
 
   private:
     std::map<std::string, EncodedAuthentication> proxyAuth_;
