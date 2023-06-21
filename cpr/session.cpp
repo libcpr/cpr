551f90973a8a1166c5765be64aa09aaad98ec9ea
Fabian Sauter <sauter.fabian@mailbox.org>
2023-06-21 08:25:20 +0200

    Fixes curl version check macros for curl >= 8.0
    Resolves #919

--- cpr/session.cpp
+++ cpr/session.cpp
@@ -136,8 +136,7 @@ void Session::prepareCommon() {
         }
     }
 
-#if LIBCURL_VERSION_MAJOR >= 7
-#if LIBCURL_VERSION_MINOR >= 21
+#if (LIBCURL_VERSION_MAJOR > 7) || (LIBCURL_VERSION_MAJOR == 7 && LIBCURL_VERSION_MINOR >= 21)
     if (acceptEncoding_.empty()) {
         /* enable all supported built-in compressions */
         curl_easy_setopt(curl_->handle, CURLOPT_ACCEPT_ENCODING, "");
@@ -145,10 +144,8 @@ void Session::prepareCommon() {
         curl_easy_setopt(curl_->handle, CURLOPT_ACCEPT_ENCODING, acceptEncoding_.getString().c_str());
     }
 #endif
-#endif
 
-#if LIBCURL_VERSION_MAJOR >= 7
-#if LIBCURL_VERSION_MINOR >= 71
+#if (LIBCURL_VERSION_MAJOR > 7) || (LIBCURL_VERSION_MAJOR == 7 && LIBCURL_VERSION_MINOR >= 71)
 #if SUPPORT_SSL_NO_REVOKE
     // NOLINTNEXTLINE (google-runtime-int)
     long bitmask{0};
@@ -164,7 +161,6 @@ void Session::prepareCommon() {
     if (noRevoke) {
         curl_easy_setopt(curl_->handle, CURLOPT_SSL_OPTIONS, CURLSSLOPT_NO_REVOKE);
     }
-#endif
 #endif
 #endif
 
