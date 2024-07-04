985581d5b20c432061f4e05f3f3a3d57475f7500
Fabian Sauter <sauter.fabian@mailbox.org>
2024-07-04 18:05:30 +0200

    Const Response::GetCertInfos()

--- include/cpr/response.h
+++ include/cpr/response.h
@@ -45,7 +45,7 @@ class Response {
 
     Response() = default;
     Response(std::shared_ptr<CurlHolder> curl, std::string&& p_text, std::string&& p_header_string, Cookies&& p_cookies, Error&& p_error);
-    std::vector<CertInfo> GetCertInfos();
+    [[nodiscard]] std::vector<CertInfo> GetCertInfos() const;
     Response(const Response& other) = default;
     Response(Response&& old) noexcept = default;
     ~Response() noexcept = default;
