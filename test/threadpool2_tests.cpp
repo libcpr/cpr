58812e1f544b37221397a28d43b7faedd28a2af5
Fabian Sauter <sauter.fabian@mailbox.org>
2025-01-26 15:15:12 +0100

    Thread pool2 tests and implementation finishing touches

--- test/threadpool2_tests.cpp
+++ test/threadpool2_tests.cpp
@@ -4,8 +4,40 @@
 
 #include "cpr/threadpool2.h"
 
-TEST(ThreadPool2Tests, StartStop) {
-    cpr::ThreadPool2 tp(1, 1);
+TEST(ThreadPool2Tests, BasicWorkOneThread) {
+    std::atomic_uint32_t invCount{0};
+    uint32_t invCountExpected{100};
+
+    {
+        cpr::ThreadPool2 tp(1, 1);
+
+        for (size_t i = 0; i < invCountExpected; ++i) {
+            tp.Submit([&invCount]() -> void { invCount++; });
+        }
+
+        // Wait for the thread pool to finish its work
+        tp.Wait();
+    }
+
+    EXPECT_EQ(invCount, invCountExpected);
+}
+
+TEST(ThreadPool2Tests, BasicWorkMultipleThreads) {
+    std::atomic_uint32_t invCount{0};
+    uint32_t invCountExpected{100};
+
+    {
+        cpr::ThreadPool2 tp(1, 10);
+
+        for (size_t i = 0; i < invCountExpected; ++i) {
+            tp.Submit([&invCount]() -> void { invCount++; });
+        }
+
+        // Wait for the thread pool to finish its work
+        tp.Wait();
+    }
+
+    EXPECT_EQ(invCount, invCountExpected);
 }
 
 int main(int argc, char** argv) {
