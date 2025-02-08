75e15c8c3538b71b08b8f71b798bb197fa9eb9f9
Fabian Sauter <fabian.sauter@apsensing.com>
2025-02-08 11:13:10 +0100

    Fixed cppcheck config as XML

--- cmake/cppcheck.cmake
+++ cmake/cppcheck.cmake
@@ -9,7 +9,7 @@ if(CMAKE_CXX_CPPCHECK)
         "--addon=y2038"
         "--std=c++${CMAKE_CXX_STANDARD}"
         "--cppcheck-build-dir=${PROJECT_BINARY_DIR}"
-        "--suppress-xml=${PROJECT_SOURCE_DIR}/CppCheckSuppressions.xml"
+        "--suppress-xml=${PROJECT_SOURCE_DIR}/cppcheck-suppressions.xml"
         "--output-file=${PROJECT_BINARY_DIR}/cppcheck.xml"
         "--check-level=normal")
 endif()
