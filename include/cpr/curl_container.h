a0e2c769184b11c2b83e458be8ffa853bd189e0f
Sebastian Muxel <sebastian@muxel.dev>
2025-09-29 15:40:18 +0200

    curl_container: add documentation to both variants
    Signed-off-by: Sebastian Muxel <sebastian@muxel.dev>

--- include/cpr/curl_container.h
+++ include/cpr/curl_container.h
@@ -40,6 +40,10 @@ class CurlContainer {
     void Add(const std::initializer_list<T>&);
     void Add(const T&);
 
+    /**
+     * Returns the URL using curl_easy_escape(...) for escaping the given parameters.
+     * Requires `CurlHolder`.
+     **/
     const std::string GetContent(const CurlHolder&) const;
 
     /**
